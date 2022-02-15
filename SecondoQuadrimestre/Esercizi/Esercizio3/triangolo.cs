14213d36437b8d404285c88cfb4dd165fbad83b5
spartacus04 <andrea.bonari.pingas@gmail.com>
2022-02-15 10:13:22 +0100

    aggiusto

--- SecondoQuadrimestre/Esercizi/Esercizio3/triangolo.cs
+++ SecondoQuadrimestre/Esercizi/Esercizio3/triangolo.cs
@@ -1,23 +1,43 @@
 using System;
 
 class Triangle {
-	private int perimeter;
+	private int side1;
+	private int side2;
+	private int side3;
 
 	public int getPerimeter() {
-		return this.perimeter;
+		return side1 + side2 + side3;
+	}
+
+	public int getSide1() {
+		return side1;
+	}
+
+	public int getSide2() {
+		return side2;
+	}
+
+	public int getSide3() {
+		return side3;
 	}
 
 	// Equilatero
 	public Triangle(int l) {
-		this.perimeter = l * 3;
+		this.side1 = l;
+		this.side2 = l;
+		this.side3 = l;
 	}
 
 	public Triangle(int l1, int l2) {
-		this.perimeter = l1 + l2 * 2;
+		this.side1 = l1;
+		this.side2 = l2;
+		this.side3 = l1;
 	}
 
 	// Scaleno
 	public Triangle(int l1, int l2, int l3) {
-		this.perimeter = l1 + l2 + l3;
+		this.side1 = l1;
+		this.side2 = l2;
+		this.side3 = l3;
 	}
 }
