c042d070f8d2a451fbb41c4b5945b36a9cb5fc09
spartacus04 <andrea.bonari.pingas@gmail.com>
2022-03-01 10:59:54 +0100

    aggiungo es8

---
+++ SecondoQuadrimestre/Esercizi/Esercizio8/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+
+namespace Esercizio8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+			do {
+				Console.WriteLine("Inserisci il numero di terreni da analizzare:");
+				N = int.Parse(Console.ReadLine());
+			} while (N < 1);
+
+			Terrain[] terrains = new Terrain[N];
+			loadArr(terrains);
+
+			do {
+				switch(menu()) {
+					case 1:
+						Terrain max = maxBuildable(terrains);
+						Console.WriteLine(max.toString());
+
+						break;
+					case 2:
+						double average = averageAnnuity(terrains);
+						Console.WriteLine("Il rendimento medio è di " + average + "€/m2");
+
+						break;
+					case 3:
+						Console.WriteLine("Inserisci l'id del terreno da cercare: ");
+						string id = Console.ReadLine();
+
+						Terrain terrain = terrainByCode(terrains, id);
+
+						if (terrain != null) {
+							Console.WriteLine("La rendita totale è di " + terrain.getTotalAnnuity() + "€");
+							Console.WriteLine("La superficie edificabile è di " + terrain.buildableMeters() + "m2");
+						}
+						else
+							Console.WriteLine("Terreno non trovato");
+
+						break;
+					case 4:
+						return;
+				}
+			} while (true);
+        }
+
+		#region loading/menu
+		static void loadArr(Terrain[] terrains) {
+			for (int i = 0; i < terrains.Length; i++) {
+				Console.WriteLine("Inserisci il codice del terreno:");
+				string id = Console.ReadLine();
+
+				Console.WriteLine("Inserisci l'estensione del terreno:");
+				int extension = int.Parse(Console.ReadLine());
+
+				Console.WriteLine("Inserisci il percentuale di terreno edificabile:");
+				int buildablePercentage = int.Parse(Console.ReadLine());
+
+				Console.WriteLine("Inserisci la rendita per metro quadro:");
+				double annuityPerSquareMeter = double.Parse(Console.ReadLine());
+
+				terrains[i] = new Terrain(id, extension, buildablePercentage, annuityPerSquareMeter);
+			}
+		}
+
+
+		static int menu() {
+			Console.WriteLine("\n\n1) Stampa il terreno con maggior numero di metri quadrati edificabili");
+			Console.WriteLine("2) Stampa la rendita media dei terreni");
+			Console.WriteLine("3) Stampa i dati di un terreno dato il suo codice");
+			Console.WriteLine("4) Esci");
+
+			int N;
+			do
+			{
+				Console.WriteLine("Scegli un'opzione:");
+				N = int.Parse(Console.ReadLine());
+			} while (N < 1 || N > 4);
+
+			return N;
+		}
+
+		#endregion
+
+		#region options
+		static Terrain maxBuildable(Terrain[] terrains) {
+			Terrain max = terrains[0];
+
+			for (int i = 1; i < terrains.Length; i++) {
+				if (terrains[i].buildableMeters() > max.buildableMeters()) {
+					max = terrains[i];
+				}
+			}
+
+			return max;
+		}
+
+		static double averageAnnuity(Terrain[] terrains) {
+			double sum = 0;
+
+			for (int i = 0; i < terrains.Length; i++) {
+				sum += terrains[i].getTotalAnnuity();
+			}
+
+			return sum / terrains.Length;
+		}
+
+		static Terrain terrainByCode(Terrain[] terrains, string code) {
+			for (int i = 0; i < terrains.Length; i++) {
+				if (terrains[i].Id == code) {
+					return terrains[i];
+				}
+			}
+
+			return null;
+		}
+
+		#endregion
+	}
+}
---
+++ SecondoQuadrimestre/Esercizi/Esercizio8/Terrain.cs
@@ -0,0 +1,57 @@
+using System;
+
+class Terrain {
+	private string id;
+	private int extension;
+	private int buildablePercentage;
+	private double annuityPerSquareMeter;
+
+	#region getters/setters
+	public string Id {
+		get { return id; }
+		set { id = value; }
+	}
+
+	public int Extension {
+		get { return extension; }
+		set { extension = value > 0 ? value : 1; }
+	}
+
+	public int BuildablePercentage {
+		get { return buildablePercentage; }
+		set { buildablePercentage = value < 0 || value > 100 ? 60 : value; }
+	}
+
+	public double AnnuityPerSQuareMeter {
+		get { return annuityPerSquareMeter; }
+		set { annuityPerSquareMeter = value < 0 ? 10 : value; }
+	}
+
+	#endregion
+
+	#region constructors
+	public Terrain(string id, int extension, int buildablePercentage, double annuity) {
+		this.id = id;
+		this.extension = extension;
+		this.buildablePercentage = buildablePercentage;
+		this.annuityPerSquareMeter = annuity;
+	}
+
+	#endregion
+
+	#region methods/functions
+	public string toString() {
+		return "Terreno: " + id + "\tEstensione: " + extension + "m2\tPercentule Edificabile: " + buildablePercentage + "%\tRendita per metro quadro: " + annuityPerSquareMeter + "€/m2";
+	}
+
+	public double getTotalAnnuity() {
+		return extension * annuityPerSquareMeter;
+	}
+
+	public double buildableMeters() {
+		return extension * buildablePercentage / 100;
+	}
+
+	#endregion
+
+}
